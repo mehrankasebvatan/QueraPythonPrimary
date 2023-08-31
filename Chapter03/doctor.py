077bf3e237cbdcdf5a741497dc148fb03de6dee6
mehrankasebvatan <mehran.kasebvatan@gmail.com>
2023-09-01 00:42:57 +0330

    doctor done

---
+++ Chapter03/doctor.py
@@ -0,0 +1,7 @@
+a, b, c = input().split()
+if a == "havij" and b == "havij":
+    print("eyes!")
+elif (a == "shalgham" and c == "shalgham") or (a == "shalgham" and b == "shalgham"):
+    print("cold!")
+else:
+    print("dead")
